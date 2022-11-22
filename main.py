c9c17f44fec614bba1c1af8480edf1d36cfdce47
savelmtr <savelmtr@gmail.com>
2022-11-22 21:08:02 +0300

    fixes case when username is empty; begin to separate viewmodel & view

--- main.py
+++ main.py
@@ -2,35 +2,28 @@ import asyncio
 import os
 import re
 
-from sqlalchemy import update, and_
+from cryptography.fernet import Fernet
+from sqlalchemy import and_, update
 from sqlalchemy.dialects.postgresql import insert
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.future import select
 from sqlalchemy.orm import aliased
+from telebot import asyncio_filters, types
 from telebot.async_telebot import AsyncTeleBot
-from telebot import types
-from telebot.asyncio_storage import StateMemoryStorage
 from telebot.asyncio_handler_backends import State, StatesGroup
-from telebot import asyncio_filters
-from models import Rooms, Users, Pairs
-import random
-from callback_texts import CALLBACK_TEXTS
-from cryptography.fernet import Fernet
-from asyncache import cached
-from cachetools import TTLCache
-from telebot.types import User as TelebotUser
+from telebot.asyncio_storage import StateMemoryStorage
 from telebot.types import Message
+from telebot.types import User as TelebotUser
 
+from callback_texts import CALLBACK_TEXTS
+from models import Pairs, Rooms, Users
+
+from viewmodel import (AsyncSession, UserCache, create_room, get_max_price, get_members, is_paired, set_pairs,
+                        get_user, get_user_info, is_admin, set_user_name_data,
+                        set_wishes, to_room_attach)
 
 NUM_PTTRN = re.compile(r'\d+')
 TOKEN = os.getenv('TOKEN')
 Encoder = Fernet(os.getenv('SECRET').encode())
-engine = create_async_engine(
-    os.getenv('PG_URI_ASYNC'),
-    echo=False
-)
-AsyncSession = async_sessionmaker(engine, expire_on_commit=False)
-UserCache = TTLCache(1024, 60)
 
 
 class ButtonStorage(StatesGroup):
@@ -81,181 +74,11 @@ async def send_welcome(message: Message, markup):
     """, reply_markup=markup)
 
 
-@cached(UserCache, lambda x: x.id)
-async def get_user(user_payload: TelebotUser) -> Users:
-    req = (
-        insert(Users)
-        .values(
-            id=user_payload.id,
-            username=user_payload.username,
-            first_name=user_payload.first_name if user_payload.first_name else '',
-            last_name=user_payload.last_name if user_payload.last_name else ''
-        )
-        .on_conflict_do_update(
-            index_elements=[Users.id],
-            set_=dict(
-                username=user_payload.username
-            )
-        )
-        .returning(Users)
-    )
-    async with AsyncSession.begin() as session:
-        q = await session.execute(req)
-        user = q.scalar()
-    return user
-
-
-async def create_room(name: str, user_payload: TelebotUser) -> int:
-    user = await get_user(user_payload)
-    req = insert(Rooms).values(
-        name=name,
-        creator_id=user.id
-    ).returning(Rooms.id)
-    async with AsyncSession.begin() as session:
-        q = await session.execute(req)
-        room_id = q.one().id
-
-        up_req = (
-            update(Users)
-            .where(Users.id == user.id)
-            .values(
-                room_id=room_id
-            )
-        )
-        await session.execute(up_req)
-        UserCache.clear()
-        return room_id
-
-
-async def to_room_attach(room_id: int, user_payload: TelebotUser) -> tuple[str | None, bool]:
-    user = await get_user(user_payload)
-    room_id_req = select(Rooms).filter(Rooms.id == room_id)
-    attaching = update(Users).where(Users.id == user.id)
-    attaching_no_pass = attaching.values(room_id=room_id, candidate_room_id=None)
-    attaching_secure = attaching.values(candidate_room_id=room_id, room_id=None)
-    UserCache.clear()
-    async with AsyncSession.begin() as session:
-        q = await session.execute(room_id_req)
-        room = q.scalar()
-        if not room:
-            return None, False
-        if room.creator_id == user.id:
-            # Создателю комнаты вводить пароль незачем.
-            await session.execute(attaching_no_pass)
-            return room.name, False
-        elif room.passkey:
-            await session.execute(attaching_secure)
-            return room.name, True
-        else:
-            await session.execute(attaching_no_pass)
-            return room.name, False
-
-
-async def set_user_name_data(name, surname, user_payload: TelebotUser) -> None:
-    user = await get_user(user_payload)
-    user_id = user.id
-    naming_req = (
-        update(Users)
-        .where(Users.id == user_id)
-        .values(
-            first_name=name,
-            last_name=surname,
-        )
-    )
-    async with AsyncSession.begin() as session:
-        await session.execute(naming_req)
-    UserCache.clear()
-
-
-async def get_user_info(user_payload, status='connect'):
-    user = await get_user(user_payload)
-    giver = aliased(Users)
-    req = (
-        select(
-            Rooms.name.label('room'),
-            Rooms.id.label('room_id'),
-            giver.first_name,
-            giver.last_name,
-            giver.username,
-            giver.wish_string.label('my_wishes'),
-        )
-        .join(Rooms, Rooms.id == giver.room_id, isouter=True)
-        .filter(
-            giver.id == user.id
-        )
-    )
-    async with AsyncSession.begin() as session:
-        q = await session.execute(req)
-        data = q.one_or_none()
-        if status == 'connect':
-            msg = CALLBACK_TEXTS.congratulations.format(
-                room=data.room,
-                my_wishes=data.my_wishes,
-                username=data.username,
-                first_name=data.first_name,
-                last_name=data.last_name
-            )
-        elif status == 'update':
-            msg = CALLBACK_TEXTS.update.format(
-                my_wishes=data.my_wishes,
-                    username=data.username,
-                    first_name=data.first_name,
-                    last_name=data.last_name
-                )
-        else:
-            msg = CALLBACK_TEXTS.info.format(
-                room=data.room,
-                my_wishes=data.my_wishes,
-                username=data.username,
-                first_name=data.first_name,
-                last_name=data.last_name
-            )
-        return msg
-
-
-async def is_admin(user_id):
-    req = (
-        select(Rooms.name)
-        .filter(Rooms.creator_id == user_id)
-    )
-    async with AsyncSession.begin() as session:
-        q = await session.execute(req)
-        result = q.scalar()
-        return True if result else False
-
-
-async def set_wishes(message, wishes):
-    req = (
-        update(Users)
-        .where(Users.id == message.from_user.id)
-        .values(
-            wish_string=wishes
-        )
-    )
-    async with AsyncSession.begin() as session:
-        await session.execute(req)
-    UserCache.clear()
-
-
-async def get_max_price(user_id):
-    req = (
-        select(Rooms.max_price)
-        .join(Users, Rooms.id == Users.room_id)
-        .filter(Users.id == user_id)
-    )
-    async with AsyncSession.begin() as session:
-        q = await session.execute(req)
-        price = q.scalar()
-        if not price:
-            return 'любая'
-        return price
-
-
-async def button_generator(user_id):
+async def button_generator(user_payload: TelebotUser):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.row(types.KeyboardButton('Участники комнаты 👥'), types.KeyboardButton('Мои данные 📋'))
     markup.row(types.KeyboardButton('Изменить имя ✍'), types.KeyboardButton('Изменить пожелания 🎀'))
-    admin = await is_admin(user_id)
+    admin = await is_admin(user_payload)
     if admin:
         markup.row(types.KeyboardButton('Сгенерировать пары 🎲'), types.KeyboardButton('Удалить всех участников ❌'))
         markup.row(types.KeyboardButton('Установить пароль 🔒'), types.KeyboardButton('Cбросить пароль 🔓'))
@@ -283,7 +106,7 @@ async def update_name(message):
 @bot.message_handler(state=ButtonStorage.update_wishes)
 async def update_wishes(message):
     wishes = message.text
-    await set_wishes(message, wishes)
+    await set_wishes(message.from_user.id, wishes)
     await bot.delete_state(message.from_user.id, message.chat.id)
     msg = await get_user_info(message.from_user, status='update')
     markup = await button_generator(message.from_user.id)
@@ -350,8 +173,12 @@ async def create_password_(message: Message):
 
 @bot.message_handler(state=ButtonStorage.connect_room)
 async def connect_room_(message: Message):
-    room_id = message.text
-    room_name, is_protected = await to_room_attach(int(room_id), message.from_user)
+    try:
+        room_id = int(message.text)
+    except ValueError:
+        await bot.reply_to(message, f'Не найдено комнаты c id:{room_id}! Попробуйте еще раз!')
+        return
+    room_name, is_protected = await to_room_attach(room_id, message.from_user)
     if not room_name:
         await bot.reply_to(message, f'Не найдено комнаты c id:{room_id}! Попробуйте еще раз!')
         await bot.set_state(message.from_user.id, ButtonStorage.connect_room, message.chat.id)
@@ -396,7 +223,7 @@ async def get_user_wishes(message: Message):
     if not payload:
         await bot.set_state(message.from_user.id, ButtonStorage.wish_list, message.chat.id)
     else:
-        await set_wishes(message, payload)
+        await set_wishes(message.from_user.id, payload)
     await bot.delete_state(message.from_user.id, message.chat.id)
     msg = await get_user_info(message.from_user)
     markup = await button_generator(message.from_user.id)
@@ -407,17 +234,6 @@ async def get_user_wishes(message: Message):
 async def enter_max_price(message):
     await set_max_price(message)
     await bot.delete_state(message.from_user.id, message.chat.id)
-
-
-async def is_in_room(room_id, user_id):
-    req = (
-        select(Users.id)
-        .filter(Users.room_id == room_id, Users.id == user_id)
-    )
-    async with AsyncSession.begin() as session:
-        q = await session.execute(req)
-        res = q.scalar()
-        return True if res else False
 
 
 @bot.message_handler(commands=['start'])
@@ -431,42 +247,36 @@ async def get_room(message: Message):
         markup.add(connect_room_btn)
         await send_welcome(message, markup)
     else:
-        in_room = await is_in_room(int(payload), message.from_user.id)
+        user = await get_user(message.from_user.id)
+        try:
+            in_room = int(payload) == user.room_id
+        except ValueError:
+            await bot.send_message(message.chat.id, f'"{payload}" не является валидным id комнаты')
+            return
         if not in_room:
             room_name, is_protected = await to_room_attach(int(payload), message.from_user)
             if is_protected:
                 await bot.send_message(message.chat.id, 'Ох-хоу-хоу 🎅'
                                                         f'Похоже комната  {room_name} запоролена.'
                                                         f'Введите пожалуйста пароль')
-                await bot.set_state(message.from_user.id, ButtonStorage.enter_password, message.chat.id)
+                await bot.set_state(user.id, ButtonStorage.enter_password, message.chat.id)
             else:
                 await bot.reply_to(message, CALLBACK_TEXTS.connect_to_room.format(room_name=room_name))
-                await bot.set_state(message.from_user.id, ButtonStorage.user_name, message.chat.id)
+                await bot.set_state(user.id, ButtonStorage.user_name, message.chat.id)
         else:
-            markup = await button_generator(message.from_user.id)
+            markup = await button_generator(user.id)
             await bot.send_message(message.chat.id, 'Хо-хо-хо! Вы вернулись!', reply_markup=markup)
-
-
-async def is_paired(user_payload):
-    req = (
-        select(Pairs.giver_id)
-        .filter(Pairs.giver_id == user_payload.id)
-    )
-    async with AsyncSession.begin() as session:
-        q = await session.execute(req)
-        res = q.scalar()
-    return True if res else False
 
 
 @bot.message_handler(content_types=['text'])
 async def button_text_handler(message):
     command = message.text
-    admin = await is_admin(message.from_user.id)
-    markup = await button_generator(message.from_user.id)
+    admin = await is_admin(message.from_user)
+    markup = await button_generator(message.from_user)
     msg = ''
     match command:
         case 'Участники комнаты 👥':
-            await show_members(message)
+            msg = await get_members(message)
             await bot.reply_to(message, msg, reply_markup=markup)
         case 'Мои данные 📋':
             paired = await is_paired(message.from_user)
@@ -483,7 +293,11 @@ async def button_text_handler(message):
             await bot.set_state(message.from_user.id, ButtonStorage.update_wishes, message.chat.id)
         case 'Сгенерировать пары 🎲':
             if admin:
-                await set_pairs(message)
+                pairs_set = await set_pairs(message.from_user)
+                if pairs_set:
+                    await bot.send_message(message.chat.id, 'Пары сгенерированы 🎀')
+                else:
+                    await bot.send_message(message.chat.id, 'Вы один-одинёшенек в комнате 😧')
             else:
                 await bot.send_message(message.chat.id, 'Упс! Вы не являетесь администратором комнаты, не шалите 😘')
         case 'Удалить всех участников ❌':
@@ -511,108 +325,6 @@ async def button_text_handler(message):
                 await bot.send_message(message.chat.id, 'Упс! Вы не являетесь администратором комнаты, не шалите 😘')
         case _:
             pass
-
-
-@bot.message_handler(commands=['wish'])
-async def set_wish(message: Message):
-    payload = message.text[6:].strip()
-    if not payload:
-        await bot.reply_to(message, f'Введите список желаемого.')
-    else:
-        user = await get_user(message.from_user)
-        req = (
-            update(Users)
-            .where(
-                Users.id == user.id
-            )
-            .values(
-                wish_string=payload
-            )
-        )
-        UserCache.clear()
-        async with AsyncSession.begin() as session:
-            await session.execute(req)
-        await bot.reply_to(message, f'В желаемое добавлено: {payload}')
-
-
-@bot.message_handler(commands=['members'])
-async def show_members(message: Message):
-    user = await get_user(message.from_user)
-    if not user.room_id:
-        await bot.reply_to(message, f'Вы не подсоединены ни к одной комнате.')
-    else:
-        room_req = (
-            select(Users)
-            .join(Rooms, Rooms.id == Users.room_id)
-            .filter(
-                Rooms.id == user.room_id
-            )
-        )
-        async with AsyncSession.begin() as session:
-            q = await session.execute(room_req)
-            members = q.scalars().all()
-        m_str = '* ' + '\n* '.join([f'@{m.username} {m.first_name} {m.last_name}' for m in members])
-        await bot.reply_to(message, m_str)
-
-
-def combine_pairs(members: list[int]):
-    used_ids = set()
-    pairs = []
-    for m in members:
-        taker = m
-        while taker == m or taker in used_ids:
-            taker = random.choice(members)
-        pairs.append((m, taker))
-        used_ids.add(taker)
-    return pairs
-
-
-@bot.message_handler(commands=['set_pairs'])
-async def set_pairs(message: Message):
-    user = await get_user(message.from_user)
-    if not user.room_id:
-        await bot.reply_to(message, f'Вы не подсоединены ни к одной комнате.')
-    else:
-        room_req = (
-            select(Users.id)
-            .join(Rooms, Rooms.id == Users.room_id)
-            .filter(
-                Rooms.id == user.room_id,
-                Rooms.creator_id == user.id
-            )
-        )
-        async with AsyncSession.begin() as session:
-            q = await session.execute(room_req)
-            members = q.scalars().all()
-        if not members:
-            await bot.reply_to(message, '''
-    Вы не являетесь создателем группы.
-    Только создатель может запускать создание пар.
-            ''')
-
-        pairs = combine_pairs(members)
-        stmt = (
-            insert(Pairs)
-            .values([
-                {
-                    'giver_id': giver,
-                    'taker_id': taker,
-                    'room_id': user.room_id
-                }
-                for (giver, taker) in pairs
-            ])
-        )
-        stmt = stmt.on_conflict_do_update(
-            index_elements=[Pairs.giver_id, Pairs.room_id],
-            set_={
-                "taker_id": stmt.excluded.taker_id
-            }
-        )
-        async with AsyncSession.begin() as session:
-            await session.execute(stmt)
-        await bot.reply_to(message, '''
-    Пары созданы.
-        ''')
 
 
 @bot.message_handler(commands=['info'])
@@ -744,7 +456,6 @@ async def rename_room(message: Message):
         await bot.reply_to(message, f'Название комнаты с id:{res.id} изменено на {res.name}')
 
 
-# @bot.message_handler(commands=['lock'])
 async def lock(message: Message):
     payload = message.text
     if not payload:
@@ -778,9 +489,6 @@ async def lock(message: Message):
 @bot.message_handler(commands=['enlock'])
 async def enlock(message: Message):
     payload = message.text
-    # if not payload:
-    #     await bot.reply_to(message, 'Вы не ввели пароль.')
-    #     return
     user = await get_user(message.from_user)
     passkey_req = (
         select(Rooms.passkey)
@@ -817,7 +525,6 @@ async def enlock(message: Message):
         await bot.set_state(message.from_user.id, ButtonStorage.user_name, message.chat.id)
 
 
-# @bot.message_handler(commands=['set_max_price'])
 async def set_max_price(message: Message):
     payload = message.text
     user = await get_user(message.from_user)
---
+++ viewmodel.py
@@ -0,0 +1,280 @@
+from asyncache import cached
+from cachetools import TTLCache
+from telebot.types import User as TelebotUser
+from sqlalchemy import update, and_
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+import os
+from models import Rooms, Users, Pairs
+from sqlalchemy.future import select
+from callback_texts import CALLBACK_TEXTS
+import random
+
+
+UserCache = TTLCache(1024, 60)
+engine = create_async_engine(
+    os.getenv('PG_URI_ASYNC'),
+    echo=False
+)
+AsyncSession = async_sessionmaker(engine, expire_on_commit=False)
+
+
+@cached(UserCache, lambda x: x.id)
+async def get_user(user_payload: TelebotUser) -> Users:
+    stmt = (
+        insert(Users)
+        .values(
+            id=user_payload.id,
+            username=user_payload.username if user_payload.username else user_payload.id,
+            first_name=user_payload.first_name if user_payload.first_name else '',
+            last_name=user_payload.last_name if user_payload.last_name else ''
+        )
+    )
+    stmt = (
+        stmt.on_conflict_do_update(
+            index_elements=[Users.id],
+            set_=dict(
+                username=stmt.excluded.username
+            )
+        )
+        .returning(Users)
+    )
+    async with AsyncSession.begin() as session:
+        q = await session.execute(stmt)
+        user = q.scalar()
+    return user
+
+
+async def create_room(name: str, user_payload: TelebotUser) -> int:
+    user = await get_user(user_payload)
+    req = insert(Rooms).values(
+        name=name,
+        creator_id=user.id
+    ).returning(Rooms.id)
+    async with AsyncSession.begin() as session:
+        q = await session.execute(req)
+        room_id = q.one().id
+
+        up_req = (
+            update(Users)
+            .where(Users.id == user.id)
+            .values(
+                room_id=room_id
+            )
+        )
+        await session.execute(up_req)
+        UserCache.clear()
+        return room_id
+
+
+async def to_room_attach(room_id: int, user_payload: TelebotUser) -> tuple[str | None, bool]:
+    user = await get_user(user_payload)
+    room_id_req = select(Rooms).filter(Rooms.id == room_id)
+    attaching = update(Users).where(Users.id == user.id)
+    attaching_no_pass = attaching.values(room_id=room_id, candidate_room_id=None)
+    attaching_secure = attaching.values(candidate_room_id=room_id, room_id=None)
+    UserCache.clear()
+    async with AsyncSession.begin() as session:
+        q = await session.execute(room_id_req)
+        room = q.scalar()
+        if not room:
+            return None, False
+        if room.creator_id == user.id:
+            # Создателю комнаты вводить пароль незачем.
+            await session.execute(attaching_no_pass)
+            return room.name, False
+        elif room.passkey:
+            await session.execute(attaching_secure)
+            return room.name, True
+        else:
+            await session.execute(attaching_no_pass)
+            return room.name, False
+
+
+async def set_user_name_data(name, surname, user_payload: TelebotUser) -> None:
+    user = await get_user(user_payload)
+    user_id = user.id
+    naming_req = (
+        update(Users)
+        .where(Users.id == user_id)
+        .values(
+            first_name=name,
+            last_name=surname,
+        )
+    )
+    async with AsyncSession.begin() as session:
+        await session.execute(naming_req)
+    UserCache.clear()
+
+
+async def get_user_info(user_payload: TelebotUser, status='connect'):
+    user = await get_user(user_payload)
+    giver = aliased(Users)
+    req = (
+        select(
+            Rooms.name.label('room'),
+            Rooms.id.label('room_id'),
+            giver.first_name,
+            giver.last_name,
+            giver.username,
+            giver.wish_string.label('my_wishes'),
+        )
+        .join(Rooms, Rooms.id == giver.room_id, isouter=True)
+        .filter(
+            giver.id == user.id
+        )
+    )
+    async with AsyncSession.begin() as session:
+        q = await session.execute(req)
+        data = q.one_or_none()
+        if status == 'connect':
+            msg = CALLBACK_TEXTS.congratulations.format(
+                room=data.room,
+                my_wishes=data.my_wishes,
+                username=data.username,
+                first_name=data.first_name,
+                last_name=data.last_name
+            )
+        elif status == 'update':
+            msg = CALLBACK_TEXTS.update.format(
+                my_wishes=data.my_wishes,
+                    username=data.username,
+                    first_name=data.first_name,
+                    last_name=data.last_name
+                )
+        else:
+            msg = CALLBACK_TEXTS.info.format(
+                room=data.room,
+                my_wishes=data.my_wishes,
+                username=data.username,
+                first_name=data.first_name,
+                last_name=data.last_name
+            )
+        return msg
+
+
+async def is_admin(user_payload: TelebotUser):
+    user = await get_user(user_payload)
+    req = (
+        select(Rooms.id)
+        .filter(
+            Rooms.creator_id == user.id,
+            Rooms.id == user.room_id
+        )
+    )
+    async with AsyncSession.begin() as session:
+        q = await session.execute(req)
+        result = q.scalar()
+        return True if result else False
+
+
+async def get_max_price(user_id: int):
+    req = (
+        select(Rooms.max_price)
+        .join(Users, Rooms.id == Users.room_id)
+        .filter(Users.id == user_id)
+    )
+    async with AsyncSession.begin() as session:
+        q = await session.execute(req)
+        price = q.scalar()
+        if not price:
+            return 'любая'
+        return price
+
+
+async def is_paired(user_payload: TelebotUser):
+    user = await get_user(user_payload)
+    req = (
+        select(Pairs.giver_id)
+        .filter(
+            Pairs.giver_id == user.id,
+            Pairs.room_id == user.room_id
+    )
+    )
+    async with AsyncSession.begin() as session:
+        q = await session.execute(req)
+        res = q.scalar()
+    return True if res else False
+
+
+async def set_wishes(user_id: int, wishes: str):
+    req = (
+        update(Users)
+        .where(Users.id == user_id)
+        .values(
+            wish_string=wishes
+        )
+    )
+    async with AsyncSession.begin() as session:
+        await session.execute(req)
+    UserCache.clear()
+
+
+async def get_members(user_payload: TelebotUser):
+    user = await get_user(user_payload)
+    if not user.room_id:
+        m_str = 'Вы не подсоединены ни к одной комнате.'
+    else:
+        room_req = (
+            select(Users)
+            .join(Rooms, Rooms.id == Users.room_id)
+            .filter(
+                Rooms.id == user.room_id
+            )
+        )
+        async with AsyncSession.begin() as session:
+            q = await session.execute(room_req)
+            members = q.scalars().all()
+        m_str = '* ' + '\n* '.join([f'@{m.username} {m.first_name} {m.last_name}' for m in members])
+    return m_str
+
+
+def combine_pairs(members: list[int]):
+    used_ids = set()
+    pairs = []
+    for m in members:
+        taker = m
+        while taker == m or taker in used_ids:
+            taker = random.choice(members)
+        pairs.append((m, taker))
+        used_ids.add(taker)
+    return pairs
+
+
+async def set_pairs(user_payload: TelebotUser) -> bool:
+    user = await get_user(user_payload)
+    room_req = (
+        select(Users.id)
+        .join(Rooms, Rooms.id == Users.room_id)
+        .filter(
+            Rooms.id == user.room_id,
+            Rooms.creator_id == user.id
+        )
+    )
+    async with AsyncSession.begin() as session:
+        q = await session.execute(room_req)
+        members = q.scalars().all()
+    if len(members) < 2:
+        return False
+
+    pairs = combine_pairs(members)
+    stmt = (
+        insert(Pairs)
+        .values([
+            {
+                'giver_id': giver,
+                'taker_id': taker,
+                'room_id': user.room_id
+            }
+            for (giver, taker) in pairs
+        ])
+    )
+    stmt = stmt.on_conflict_do_update(
+        index_elements=[Pairs.giver_id, Pairs.room_id],
+        set_={
+            "taker_id": stmt.excluded.taker_id
+        }
+    )
+    async with AsyncSession.begin() as session:
+        await session.execute(stmt)
+    return True
