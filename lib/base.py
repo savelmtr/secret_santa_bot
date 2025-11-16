4d1be65eeca431dc9b6461968fbf0fd4bbd5cd28
savelmtr <savelmtr@gmail.com>
2025-11-16 21:17:45 +0300

    test

--- lib/base.py
+++ lib/base.py
@@ -74,6 +74,7 @@ class CustomBot(AsyncTeleBot):
         *args,
         **kwargs
     ) -> Message:
+        print("sending", chat_id, text)
         reply_markup = reply_markup or await self.get_available_buttonset(chat_id)
         await super().send_message(
             chat_id,
@@ -91,4 +92,5 @@ class CustomBot(AsyncTeleBot):
         )
 
     def add_message_handler(self, handler_dict: dict):
+        print("add_message_handler", handler_dict)
         self.message_handlers.insert(-1, handler_dict)
